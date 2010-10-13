7a9271e46bf4b9ceae5b963674c385213bd875d2
Tiago Samahá Cordeiro <tiagosamaha@gmail.com>
2010-10-13 15:20:53 -0300

    Atualizado path do buildout.

--- helper_scripts/install_digital_library.sh
+++ helper_scripts/install_digital_library.sh
@@ -2,5 +2,5 @@
 
 FOLDER="$(cd $(dirname $0); pwd)"
 
-python2.4 $FOLDER/biblioteca_digital/bootstrap.py
-$FOLDER/biblioteca_digital/bin/buildout -v
+python2.4 $FOLDER/../biblioteca_digital/bootstrap.py
+$FOLDER/../biblioteca_digital/bin/buildout -v
