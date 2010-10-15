c68f1625125f64c65f3fdf079452a706df2f441d
Tiago Samahá Cordeiro <tiagosamaha@gmail.com>
2010-10-15 08:50:09 -0300

    Alterado timeout do pound. Alterada ordem de instalação do setuptools.

--- helper_scripts/install_digital_library.sh
+++ helper_scripts/install_digital_library.sh
@@ -2,13 +2,13 @@
 
 FOLDER="$(cd $(dirname $0); pwd)"
 
-cd $FOLDER/../biblioteca_digital/
-
-python2.4 $FOLDER/../biblioteca_digital/bootstrap.py
-$FOLDER/../biblioteca_digital/bin/buildout -v
-
 wget -O /tmp/ez_setup.py http://peak.telecommunity.com/dist/ez_setup.py
 python2.4 /tmp/ez_setup.py
 easy_install-2.4 pip
 pip install http://pascal.iff.edu.br/pypi/PIL-1.1.6.tar.gz
 pip install extractor
+
+cd $FOLDER/../biblioteca_digital/
+
+python2.4 $FOLDER/../biblioteca_digital/bootstrap.py
+$FOLDER/../biblioteca_digital/bin/buildout -v
